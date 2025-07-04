e66fa8144773aa47781625fcf4b1a2c95797b9d2
Roberto Alemán <alemanbarahona@gmail.com>
2025-07-03 21:15:26 -0600

    Update patrones.py

--- patrones.py
+++ patrones.py
@@ -16,3 +16,19 @@ def detectar_patron_5velas(df):
         elif colores == ['verde', 'roja', 'verde', 'roja', 'verde']:
             df.loc[i+1, 'prediccion_sexta'] = 'PUT'
     return df
+
+
+def detectar_envolventes(df):
+    df['alcista'] = (
+        (df['close'] > df['open']) &
+        (df['open'].shift(1) > df['close'].shift(1)) &
+        (df['close'] > df['open'].shift(1)) &
+        (df['open'] < df['close'].shift(1))
+    )
+    df['bajista'] = (
+        (df['close'] < df['open']) &
+        (df['open'].shift(1) < df['close'].shift(1)) &
+        (df['close'] < df['open'].shift(1)) &
+        (df['open'] > df['close'].shift(1))
+    )
+    return df
